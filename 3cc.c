80100b3f4818e070806f9e3c473ae7413bbd16c5
Yuanxiang Ye <alvin.ye.y@gmail.com>
2021-11-07 13:25:22 -0600

    Added addtion and subtraction

--- 3cc.c
+++ 3cc.c
@@ -7,10 +7,30 @@ int main(int argc, char** argv) {
     return 1;
   }
 
+  char* p = argv[1];
+
   printf("format PE64\n");
   printf("section '.text' code readable executable\n");
-  printf("start:\n");
-  printf("\tmov rax, %d\n", atoi(argv[1]));
-  printf("\tret\n");
+  printf("main:\n");
+  printf("  mov rax, %d\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "Invalid character: '%c'\n", *p);
+    return 1;
+  }
+
+  printf("  ret\n");
   return 0;
 }
